158a9bb838e450ce7ed108f93da2654796ac11f0
oussama-talaoui <tahiri.oussup@gmail.com>
2020-06-13 21:06:27 +0200

    Added files

---
+++ dbpedia-cron.sh
@@ -0,0 +1,89 @@
+echo "Creating a cron job for updating Spanish Dbpedia."
+
+read -p "Enter month ( 1 to 12 from January to December, empty or * for ever month): " month
+while [[ ! $month =~ ^[0-9]+$ || ! $month -ge 1 || ! $month -le 12 ]]
+do
+    if [[ $month == '' || $month == '*' ]]
+	then
+		month='*'
+	break
+    fi
+    printf 'Please enter a valide month number: '
+    read month
+done
+
+read -p "Enter weekday (0 to 6 from Sunday to Saturday, empty or * for ever weekday): " weekday
+while [[ ! $weekday =~ ^[0-9]+$ || ! $weekday -ge 0 || ! $weekday -le 6 ]]
+do
+    if [[ $weekday == '' || $weekday == '*' ]]
+	then
+		weekday='*'
+	break
+    fi
+    printf 'Please enter a valide weekday: '
+    read weekday
+done
+
+read -p "Enter day of the month (1 to 31, empty or * for everyday): " day
+while [[ ! $day =~ ^[0-9]+$ || ! $day -ge 1 || ! $day -le 31 ]]
+do
+    if [[ $day == '' || $day == '*' ]]
+	then
+		day='*'   
+	break
+    fi
+    printf 'Please enter a valide day: '
+    read day
+done
+
+read -p "Enter hour of the day (0 to 24, empty or * for every hour): " hour
+while [[ ! $hour =~ ^[0-9]+$ || ! $hour -ge 0 || ! $hour -le 24 ]]
+do
+    if [[ $hour == '' || $hour == '*' ]]
+	then
+		hour='*'
+	break
+    fi
+    printf 'Please enter a valide hour: '
+    read hour
+done
+
+read -p "Enter minute (0 to 59, empty or * for every minute): " minute
+while [[ ! $minute =~ ^[0-9]+$ || ! $minute -ge 0 || ! $minute -le 59 ]]
+do
+    if [[ $minute == '' || $minute == '*' ]]
+	then
+		minute='*'
+	break
+    fi
+    printf 'Please enter a valide minute: '
+    read minute
+done
+
+timestamp='$(date "+\%Y.\%m.\%d-\%H.\%M.\%S")'
+# Backslash used to escape % which is a new line in ts command (ts from moreutils: $ sudo apt-get install moreutils)
+cronjob="$minute $hour $day $month $weekday /home/zinner/Desktop/update-script.sh | ts '[\%Y-\%m-\%d \%H:\%M:\%S]' >> /home/zinner/Desktop/"$timestamp"_esdbpedia-update.log 2>&1; echo $? | ts '[\%Y-\%m-\%d \%H:\%M:\%S]' >> /home/zinner/Desktop/"$timestamp"_code-error.log"
+
+cronexists='echo Cron job for this file already exists!
+read -p "Do want to remove it (y/n)? " -n 1 -r
+echo 
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+else
+    crontab -r
+fi
+read -p "Do want to create this new cron job (y/n)? " -n 1 -r
+echo 
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+else
+    { echo "$cronjob"; } | crontab -
+fi
+'
+
+chmod +x /home/zinner/Desktop/update-script.sh
+
+echo "Your cron job command is: $cronjob"
+crontab -l | grep -q '/home/zinner/Desktop/update-script.sh' && eval "$cronexists" || { echo "$cronjob"; } | crontab -
---
+++ update-script.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+# TODO
+# Graphic interface
+# More variables
+# User friendly script
+
+#--------------------- Start of the script ---------------------
+# Along the execution of our script we calculate the time taken
+start=`date +%s`
+
+# variables definition
+DATAPATH="vad"
+GRAPH="<http://es.dbpedia.org>"
+ACCOUNT="dba"
+PASSWORD="dba"
+
+# post request to a sparql end point using content in txt file and write the result to a text file as list
+QUERY=$(</home/zinner/Desktop/request.txt)
+
+# retrieve downloadurls with sparql query
+DOWNLOADURLS=`curl -X POST --data-urlencode query="$QUERY" --data-urlencode format="text/tab-separated-values"  "https://databus.dbpedia.org/repo/sparql"`
+
+# remove double quotes " from downloadurls, because of wget scheme missing
+DOWNLOADURLS=`echo $DOWNLOADURLS| sed 's/"//g'`
+echo $DOWNLOADURLS
+
+# remove previous data files
+echo -n 'Deleting files... '
+rm /usr/local/virtuoso/share/virtuoso/"$DATAPATH"/* & PID=$!
+echo 'Done!'
+
+# download files and append timestamp
+for url in $DOWNLOADURLS; do
+	echo "Downloading" $url
+	current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+	#echo "Current Time : $current_time"
+	filename=$(basename "$url")
+	wget -q -c $url -O /usr/local/virtuoso/share/virtuoso/"$DATAPATH"/"$current_time.$filename" &
+done
+wait
+
+# remove uneeded files
+echo "Removing uneeded files."
+rm /usr/local/virtuoso/share/virtuoso/"$DATAPATH"/*.file
+rm /usr/local/virtuoso/share/virtuoso/"$DATAPATH"/*.pdf
+
+# uncompress the bz2 files and delet them
+printf "Uncompressing .bz2 files..."
+ls /usr/local/virtuoso/share/virtuoso/"$DATAPATH"/*.bz2 | parallel bunzip2 & PID=$!
+echo 'Done!'
+
+# rm /usr/local/virtuoso/var/lib/virtuoso/db/virtuoso.lck
+
+# runing the virtuoso server if it's down and waiting for it to run before continuing
+if netstat -tulpn | grep :8890
+then
+    echo "server runing"
+else
+    echo "running the server..."
+    sudo virtuoso-t -f -c /usr/local/virtuoso/var/lib/virtuoso/db/virtuoso.ini
+    while ! netstat -tulpn | grep :8890
+    do
+	wait
+    done
+    echo "server runing"
+fi
+
+# Create graph name file
+echo "http://es.dbpedia.org" > /usr/local/virtuoso/share/virtuoso/vad/global.graph
+
+# Clear the graph and the load list table, to load new data
+# Large graphs can be cleared by changing the transaction log mode to autocommit on each operation, deleting the graph(s) or triples. This is easily done using the Virtuoso log_enable function, with the settings log_enable(3,1).
+
+isql-v 1111 dba dba exec="log_enable(3,1);"
+# isql-v 1111 dba dba exec="DELETE FROM rdf_quad WHERE g = iri_to_id ('http://es.dbpedia.org');"
+isql-v 1111 dba dba exec="SPARQL CLEAR GRAPH <http://es.dbpedia.org>;"
+isql-v 1111 dba dba exec="DELETE FROM DB.DBA.load_list;"
+isql-v 1111 dba dba exec="ld_dir ('/usr/local/virtuoso/share/virtuoso/vad', '*.*', 'http://es.dbpedia.org');"
+
+# Getting number of cores in the machine and running multiple Loaders using no cores / 2.5, to optimally parallelize the data load.
+noperation=`echo $(nproc) / 2.5 | bc`
+echo $noperation
+echo "Running multiple Loaders:"
+for (( c=1; c<=$noperation; c++ ))
+do
+	isql-v 1111 dba dba exec="rdf_loader_run();" &
+done
+wait
+
+isql-v 1111 dba dba exec="checkpoint;"
+# Showing the time taken to load the datasets to our graph
+isql-v 1111 dba dba exec="select min(ll_started) as start, max(ll_done) as finish, datediff('second', min(ll_started), max(ll_done)) as delta from load_list where ll_graph like 'http://es.dbpedia.org';"
+isql-v 1111 dba dba exec="SPARQL SELECT (COUNT(*) as ?Triples) FROM <http://es.dbpedia.org> WHERE { ?s ?p ?o };"
+isql-v 1111 dba dba exec="SPARQL SELECT *  FROM <http://es.dbpedia.org> WHERE    {     ?s ?p ?o   } LIMIT 100 ;"
+
+end=`date +%s`
+runtime=$((end-start))
+echo Runing the script took $(($runtime / 60)) minutes.
